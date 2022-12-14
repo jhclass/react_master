52d86e62f60e356f14a4fad3c70a7c5f8224ef0f
Jinhyoung Lee <jhcode0@naver.com>
2022-12-14 12:00:51 +0900

    초기화 : theme

--- src/App.js
+++ src/App.js
@@ -1,104 +1,11 @@
-import styled,{keyframes} from "styled-components";
-
-const Father = styled.div`
-display:flex
-`;
-
-//props 확장
-const Box = styled.div`
-background-color:${(props) =>props.bgColor};
-width:300px;
-height:300px;
-margin-right:10px;
-`
-//확장
-const Circle = styled(Box)`
-/*background-color:${(props) =>props.bgColor};
-width:300px;
-height:300px;
-margin-right:10px;*/
-border-radius:100%;
-`
-
-const Text = styled.span`
- color:#fff;
-`
-
-const Btn = styled.button`
-width:50px; 
-height:30px;
-border-radius:10px;
-background-color:${(props)=>props.bgColor};
-text-align:Center;
-`
-const Input = styled.input.attrs({required:true})`
-background-color:#eee;
-`;
-
-const Wrapper = styled.div`
-display:flex
-`;
-
-const box3_animation = keyframes`
-from {
-  transform:rotate(0deg);
-  border-radius : 0px;
-}
-to {
-  transform:rotate(360deg);
-  border-radius: 100px;
-}
-`
-
-const Emoji = styled.span`
-font-size:30px;
-opacity:1;
-color:#333;
-text-decoration:none;
-`;
+import styled from "styled-components";
 
 
-const Box3 = styled.div`
-height:200px;
-width:200px;
-display:flex;
-justify-content:center;
-align-items:center;
-border-radius:30px;
-background-color:tomato;
-animation:${box3_animation} 1s linear infinite;
-cursor:pointer;
 
-${Emoji} {
-    
-  &:hover{
-    font-size:40px;
-  }
-
-}
-`;
 function App() {
+
   return (
-    <>
-    <Father>
-      <Box bgColor="red"><Text>Box1</Text></Box>
-      <Circle bgColor="blue"><Text>Box2</Text></Circle>
-      <Btn bgColor="green">Login</Btn>
-      <Btn bgColor="pink" as="a" href="/">Link</Btn>
-      <hr/>
-      <Input/>
-      <Input/>
-      <Input/>
-      <Input/>
-      <Input/>
-    </Father>
-    <Wrapper>
-      <Box3><Emoji as="a" href="naver.com">Wow!</Emoji></Box3>
-     
-     
-    </Wrapper>
-    </>
-    
+   <div></div>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components';
 import App from './App';
 
-const darkTheme = {
-  textColor: "whitesmoke",
-  backgroundColor: "#111",
-};
-
-const lightTheme = {
-  textColor: "#111",
-  backgroundColor: "whitesmoke",
-};
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
       <App/>
-    </ThemeProvider>
-    
   </React.StrictMode>
 );
-
-
