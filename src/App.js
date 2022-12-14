6240d303e156c8fca0af50b1f11d4f423b98869d
Jinhyoung Lee <jhcode0@naver.com>
2022-12-14 12:26:47 +0900

    ThemeProvider 적용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import styled from "styled-components";
 
+const Title  = styled.h1`
+color:tomato;
+`;
+
+const Wrapper = styled.div`
+display:flex;
+height:100vh;
+width:100vw;
+justify-content:center;
+align-items:center;
+background-color:${props=>props.theme}
+`;
 
 
 function App() {
 
   return (
-   <div></div>
+   <Wrapper>
+      <Title>hello!</Title>
+   </Wrapper>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {ThemeProvider} from 'styled-components';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const darkTheme = {
+  textColor:"smokewhite",
+  backgroundColor:"#111"
 
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
       <App/>
+    </ThemeProvider>
+    
   </React.StrictMode>
 );
