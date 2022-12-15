ab0193f88215bee7078017ca43de7ed81f492aba
Jinhyoung Lee <jhcode0@naver.com>
2022-12-15 16:45:58 +0900

    useState() 시작

--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,30 +1,30 @@
+import React,{useState} from 'react';
 import styled from 'styled-components';
-
 
 interface ContainerProps {
     cbgColor:string;
     bColor:string;
 }
-
 const Container = styled.div<ContainerProps>`
     width:200px;
     height:200px;
     background-color:${props=>props.cbgColor};
     border-radius:100%;
-
 `;
 interface CircleProps {
    bgColor:string;
    borderColor?:string; //borderColor가 required 된 상태이기 때문에 Container 속성에 ?? 를 넣어야 한다.
+    text?:string
 }
 
 
-function Circle({bgColor,borderColor}:CircleProps) {
+function Circle({bgColor,borderColor, text="default text"}:CircleProps) {
+
+   
+  //alert(setValue(1));
     return (
-    <Container cbgColor={bgColor} bColor={borderColor ?? bgColor}>box</Container>
+    <Container cbgColor={bgColor} bColor={borderColor ?? bgColor}></Container>
     );
 }
 
-
 export default Circle;
-
