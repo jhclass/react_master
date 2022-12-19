2436a27da454641f01efbc4d41ac919fafdc4747
Jinhyoung Lee <jhcode0@naver.com>
2022-12-19 17:37:59 +0900

    초기화 : typescript + styledComponent

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,8 @@ import styled from "styled-components"
 
 import './App.css';
 
-
-
 function App() {
-  const [value,setValue] = useState("");
-  const onChange = (event:React.FormEvent<HTMLInputElement>) => {
-    //console.log(event.currentTarget.value);
-    const {currentTarget: {value}} = event;
-    //const value = event.currentTarget.value;
-    setValue(value);
-  }
-  const onSubmit = (event:React.FormEvent<HTMLFormElement>)=>{
-    event.preventDefault();
-    console.log('hello');
-  }
+ 
   return (
     <div>
       <form onSubmit={onSubmit}>
