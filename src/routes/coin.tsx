ae1fbbc0588fdb61f7d33501b72e8e8672cf9798
Jinhyoung Lee <jhcode0@naver.com>
2022-12-19 20:03:11 +0900

    typescript : useParams()

--- src/routes/coin.tsx
+++ src/routes/coin.tsx
@@ -1,6 +1,10 @@
-
-
+import {useParams} from "react-router";
+interface Params{
+    coinId:string;
+}
 function Coin(){
+    const {coinId} = useParams<Params>();
+    //console.log(params);
     return(
         <h1></h1>
     );
