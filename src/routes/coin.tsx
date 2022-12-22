d85477a1ee82b47da32abaf2380c8910cc30de0d
Jinhyoung Lee <jhcode0@naver.com>
2022-12-22 16:51:24 +0900

    코인데이터 받고 타입스크립트 인터페이스 정의 후 지정한 데이터 출력

--- src/routes/coin.tsx
+++ src/routes/coin.tsx
@@ -1,9 +1,7 @@
-import {useLocation, useParams} from "react-router";
+import {useLocation,useParams} from "react-router";
 import React,{useEffect, useState} from 'react';
 import styled from "styled-components";
-interface Params{
-    coinId:string;
-}
+
 const Container = styled.div`
 padding:20px 20px;
 max-width:480px;
@@ -16,7 +14,8 @@ const Header = styled.header`
     align-items:center;
 `;
 const Title = styled.h1`
- font-family: 'Source Code Pro', monospace;
+ font-family: Source Code Pro
+ monospace;
  color:${props=>props.theme.bgColor};
  font-size:26px;
  font-weight:bold;
@@ -30,10 +29,63 @@ const Title = styled.h1`
  font-size:30px;
  padding-top:20px;
  `;
+
+ interface Params{
+    coinId:string;
+}
+
  interface RouteState{
    name:string;
+ }
+ 
+ interface InfoData {
+    id: string; //0
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;//6
+    logo: string;
+    tags: object; //8
+    team: object; //9
+    description: string; //10
+    message: object; //11
+    open_source: boolean; //12
+    started_at: string; //13
+    development_status: string; //14
+    hardware_wallet: boolean;//15
+    proof_type: string; //16
+    org_structure: string; //17
+    hash_algorithm: string; //18
+    links: object; //19
+    links_extended: object; //20
+    whitepaper: object;
+    first_data_at: string;
+    last_data_at: string;
 
  }
+ 
+ 
+ interface PriceData {
+
+ id : string; //1
+ name : string;
+ symbol : string;
+ rank : string;
+ price_usd : string;
+ price_btc : string;
+ volume_24h_usd : string;
+ market_cap_usd : string;
+ circulating_supply : string;
+ total_supply : string;
+ max_supply : string;
+ percent_change_1h : string;
+ percent_change_24h : string;
+ percent_change_7d : string;
+ last_updated : string;
+ }
+
 function Coin(){
 
     const {coinId} = useParams<Params>();
@@ -41,27 +93,29 @@ function Coin(){
     const [loading,setLoading] = useState(true);
     const {state} = useLocation<RouteState>();
     //console.log(state.name); // 어떤 정보가 전달이 되었나 확인해보자
-    const [info,setInfo] = useState({});
-    const [priceInfo,setPriceInfo] = useState({});
+    const [info, setInfo] = useState<InfoData>();
+    const [priceInfo, setPriceInfo] = useState<PriceData>();
     useEffect(()=>{
        (
         async()=>{
-            //캡슐화
-            const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
-            //console.log(coinData);
-            const priceData = await(await fetch(`https://api.coinpaprika.com/v1/ticker/${coinId}`)).json();
-           // console.log(priceData);    
-           setInfo(infoData);
+         //캡슐화
+        const infoData = await (await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)).json();
+        console.log(infoData);
+        const priceData = await(await fetch(`https://api.coinpaprika.com/v1/ticker/${coinId}`)).json();
+        console.log(priceData);    
+        setInfo(infoData);
            setPriceInfo(priceData);
+           setLoading(false);
         }
             
         )(); //바로실행가능한 함수! ()
 
     },[]);
+
     return(
         <Container>
             <Header><Title>{state?.name || "Loading"}</Title></Header>
-            {loading ? (<Loader>Loading...</Loader>) : null }
+            {loading ? (<Loader>Loading...</Loader>) :  priceInfo?.price_btc}
         </Container>
     );
 }
