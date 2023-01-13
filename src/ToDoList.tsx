add6b8ac756b027e7f0c239f4d2e702c321fee8b
Jinhyoung Lee <jhcode0@naver.com>
2023-01-13 17:14:13 +0900

    React-hook-form : register() && watch()

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,31 +1,53 @@
 import { useState } from "react";
+import {useForm} from "react-hook-form";
 
+
+// function ToDoList() {
+//     const [toDo,setTodo] = useState("");
+//     const [toDoError,setToDoError] = useState("");
+//     const onChange = (event:React.FormEvent<HTMLInputElement>)=>{ // :any 를 안쓰면 value 라는 값을 못찾는다.
+//         // console.log(event.target.value);
+//         // const {value}=event.target
+//         const {currentTarget:{value},}=event;
+//         setTodo(value);
+//     }
+//     const onSubmit = (event:React.FormEvent<HTMLFormElement>)=>{
+//         event.preventDefault();
+//         if (toDo.length < 10) {
+//             setToDoError("좀 더 길게 작성해보세요...");
+//             return console.log(toDoError);
+//         }
+//         console.log('submit');
+//     }
+//     return (
+        
+//         <div>
+//             <form onSubmit={onSubmit}>
+//                 <input value={toDo} onChange={onChange}placeholder="Write a to do"/>
+//                 <button type="submit">Add</button>
+//             </form>
+//         </div>
+//     );
+// }
 
 function ToDoList() {
-    const [toDo,setTodo] = useState("");
-    const [toDoError,setToDoError] = useState("");
-    const onChange = (event:React.FormEvent<HTMLInputElement>)=>{ // :any 를 안쓰면 value 라는 값을 못찾는다.
-        // console.log(event.target.value);
-        // const {value}=event.target
-        const {currentTarget:{value},}=event;
-        setTodo(value);
-    }
-    const onSubmit = (event:React.FormEvent<HTMLFormElement>)=>{
-        event.preventDefault();
-        if (toDo.length < 10) {
-            setToDoError("좀 더 길게 작성해보세요...");
-            return console.log(toDoError);
-        }
-        console.log('submit');
-    }
+    const { register, watch} = useForm();
+    //console.log(register("To Do"));
+    console.log(watch())
     return (
-        
         <div>
-            <form onSubmit={onSubmit}>
-                <input value={toDo} onChange={onChange}placeholder="Write a to do"/>
-                <button type="submit">Add</button>
+            <form>
+                <input {...register("email")} placeholder="Email"/>
+                <input {...register("firstName")} placeholder="First name"/>
+                <input {...register("lastName")} placeholder="Last name"/>
+                <input {...register("userName")} placeholder="User name"/>
+                <input {...register("Password")} placeholder="Password"/>
+                <button>Add</button>
             </form>
         </div>
     );
 }
+
+
+
 export default ToDoList;
