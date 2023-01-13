e5f2c5b768df12017d3e0a47edac851a4eb21211
Jinhyoung Lee <jhcode0@naver.com>
2023-01-13 15:18:27 +0900

    Form 요소를 만들때 작성하는 지극히 레거시한 방법 ㅡㅡ;

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 
 function ToDoList() {
-    return null;
+    const [toDo,setTodo] = useState("");
+    const onChange = (event:React.FormEvent<HTMLInputElement>)=>{ // :any 를 안쓰면 value 라는 값을 못찾는다.
+        // console.log(event.target.value);
+        // const {value}=event.target
+        const {currentTarget:{value},}=event;
+        setTodo(value);
+    }
+    const onSubmit = (event:React.FormEvent<HTMLFormElement>)=>{
+        event.preventDefault();
+        console.log(toDo);
+    }
+    return (
+        
+        <div>
+            <form onSubmit={onSubmit}>
+                <input value={toDo} onChange={onChange}placeholder="Write a to do"/>
+                <button type="submit">Add</button>
+            </form>
+        </div>
+    );
 }
 export default ToDoList;
