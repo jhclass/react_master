912e5f42eccc723885309e5b64a2dc3813abe612
jhclass <jhcode0@naver.com>
2023-01-23 01:46:51 +0900

    onClick 시 인자를 받는 방법 2

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,17 +1,39 @@
-import { IToDo } from "../atoms";
+import React from "react";
+import { useRecoilState } from "recoil";
+import { IToDo,toDoState } from "../atoms";
 
-function ToDo({text,category}:IToDo) {
-    const onClick = (newCategory:IToDo["category"])=>{
-        console.log(newCategory);
+function ToDo({text,category,id}:IToDo) {
+    const setToDos = useRecoilState(toDoState);
+    // const onClick = (newCategory:IToDo["category"])=>{
+    //     console.log(newCategory);
+    // }
+    
+    const onClick = (event:React.MouseEvent<HTMLButtonElement>)=>{
+        const {currentTarget:{name}} =event
+        console.log(name);
     }
     return (
     <li>
         <span>{text}</span>
-        {category !== "TO_DO" && <button onClick={()=>onClick("TO_DO")}>To Do</button>}
-        {category !== "DOING" && <button onClick={()=>onClick("DOING")}>Doing</button>}
-        {category !== "DONE" && <button onClick={()=>onClick("DONE")}>Done</button>}
+        {category !== "TO_DO" && <button name="TO_DO" onClick={onClick}>To Do</button>}
+        {category !== "DOING" && <button name="DOING" onClick={onClick}>Doing</button>}
+        {category !== "DONE" && <button name="DONE" onClick={onClick}>Done</button>}
     </li>
     );
 }
+
+// function ToDo({text,category}:IToDo) {
+//     const onClick = (newCategory:IToDo["category"])=>{
+//         console.log(newCategory);
+//     }
+//     return (
+//     <li>
+//         <span>{text}</span>
+//         {category !== "TO_DO" && <button onClick={()=>onClick("TO_DO")}>To Do</button>}
+//         {category !== "DOING" && <button onClick={()=>onClick("DOING")}>Doing</button>}
+//         {category !== "DONE" && <button onClick={()=>onClick("DONE")}>Done</button>}
+//     </li>
+//     );
+// }
 
 export default ToDo;
