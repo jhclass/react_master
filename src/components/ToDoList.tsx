df895032a935bc533164cca2512b81e6c01bbfe5
jhclass <jhcode0@naver.com>
2023-01-22 16:13:34 +0900

    useRecoilState()

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,14 +1,33 @@
 import {useForm} from "react-hook-form";
+import { atom, useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
+
 
 interface IForm {
     toDo:string;
 }
+interface IToDo {
+    text:string;
+    category: "TO_DO" | "DOING" | "DONE";
+    id:number;
+}
+
+const toDoState = atom<IToDo[]>({
+    key: "toDo",
+    default:[],
+
+});
+
 function ToDoList() {
+    const [toDos,setToDos] = useRecoilState(toDoState); // 두줄로 쓰는것보다 한줄로 쓰는게 낫지 , useState랑 비슷하네?
+    //const value = useRecoilValue(toDoState);
+   // const modFn = useSetRecoilState(toDoState);
     const {register, handleSubmit, setValue} = useForm<IForm>();
-    const onValid = (data:IForm)=>{
-        console.log(data.toDo);
-        setValue("toDo","");
+    const onValid = ({toDo}:IForm)=>{
+        console.log(toDo);
+        setToDos(oldToDos=> [{text:toDo, id:Date.now(), category:"TO_DO"},...oldToDos])
+        setValue("toDo","");   
     }
+    console.log(toDos);
     
     return (
         <div>
@@ -18,7 +37,9 @@ function ToDoList() {
                 <input {...register("toDo",{required:"Please write a Todo"})} placeholder="Write a to do"/>
                 <button>add</button>
             </form>
-            <ul></ul>
+            <ul>
+                {toDos.map((toDo)=><li key={toDo.id}>{toDo.text}</li>) }
+            </ul>
         </div>
     );
 }
