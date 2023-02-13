ecc4630c4cc639b0915a1ab4112a4ed581e58446
Jinhyoung Lee <jhcode0@naver.com>
2023-02-13 16:55:31 +0900

    초기화

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-
 import styled from 'styled-components';
 import { motion } from "framer-motion";
-
 
 const Wrapper = styled.div`
   display: flex;
@@ -13,51 +11,24 @@ const Wrapper = styled.div`
   align-items: center;
   height: 100vh;
 `;
+
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   display:grid;
   grid-template-columns: repeat(2,1fr);
 
-  background-color: rgba(255,255,255,0.2);
+  background-color: #fff;
   border-radius: 15px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
-const Circle = styled(motion.div)`
-  background-color:white;
-  height:70px;
-  width:70px;
-  place-self: center;
-  border-radius:35px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
 const boxVariants = {
-  start:{scale:0,opacity:0},
-  end:{scale:1,opacity:1,
-    transition:{
-      duration:0.5,type:"spring",bounce:0.5,
-      delayChildredn:0.5,
-      staggerChildren:0.5,
-      
-      
-    }
- },
+  
   
 }
 
-const childrenVarients = {
-  start:{
-    opacity:0,
-    y:-10,
-    
-  },
-  end:{
-    opacity:1,
-    y:0,
-    transition:{duration:1}
-  },
-}
+
 
 
 
@@ -68,14 +39,8 @@ function App() {
   //console.log('aaaa',addB);
   return (
   <Wrapper>
-    <Box variants={boxVariants} initial="start" animate="end">
-      <Circle variants={childrenVarients}/>
-      <Circle variants={childrenVarients}/>
-      <Circle variants={childrenVarients}/>
-      <Circle variants={childrenVarients}/>
-      
-    </Box>
-
+    <Box />
+  
   </Wrapper>
   
   );
