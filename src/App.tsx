ca17c4bf143bb67a903bf295e42604b423e42b82
Jinhyoung Lee <jhcode0@naver.com>
2023-02-13 14:24:42 +0900

    basic animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,7 @@
 
 import styled,{createGlobalStyle} from 'styled-components';
 import { motion } from "framer-motion";
-const GlobalStyle = createGlobalStyle`
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;500;700;900&display=swap');
-@import url('https://fonts.googleapis.com/css2?family=Source+Code+Pro:wght@500&display=swap');
-html, body, div, span, applet, object, iframe,
-h1, h2, h3, h4, h5, h6, p, blockquote, pre,
-a, abbr, acronym, address, big, cite, code,
-del, dfn, em, img, ins, kbd, q, s, samp,
-small, strike, strong, sub, sup, tt, var,
-b, u, i, center,
-dl, dt, dd, menu, ol, ul, li,
-fieldset, form, label, legend,
-table, caption, tbody, tfoot, thead, tr, th, td,
-article, aside, canvas, details, embed,
-figure, figcaption, footer, header, hgroup,
-main, menu, nav, output, ruby, section, summary,
-time, mark, audio, video {
-  margin: 0;
-  padding: 0;
-  
-  border: 0;
-  font-size: 100%;
-  font: inherit;
-  vertical-align: baseline;
-}
-/* HTML5 display-role reset for older browsers */
-article, aside, details, figcaption, figure,
-footer, header, hgroup, main, menu, nav, section {
-  display: block;
-}
-/* HTML5 hidden-attribute fix for newer browsers */
-*[hidden] {
-    display: none;
-}
 
-menu, ol, ul {
-  list-style: none;
-}
-blockquote, q {
-  quotes: none;
-}
-blockquote:before, blockquote:after,
-q:before, q:after {
-  content: '';
-  content: none;
-}
-table {
-  border-collapse: collapse;
-  border-spacing: 0;
-}
-body {
-
- font-weight: 300;
-  font-family: 'Source Sans Pro', sans-serif;
- 
-  color:black;
-  line-height: 1.2;
-  background:linear-gradient(135deg,#e09,#d0e);
-}
-a {text-decoration:none;}
-*{box-sizing:border-box;}
-`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -73,13 +13,19 @@ const Wrapper = styled.div`
   align-items: center;
   height: 100vh;
 `;
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+const myVars = {
+  start: {scale:0},
+  end:{scale:1, rotateZ:360, transition:{delay:1,duration:1, type:"spring", damping:10}}
+}
+
+
 function App() {
  
 
@@ -87,7 +33,8 @@ function App() {
   //console.log('aaaa',addB);
   return (
   <Wrapper>
-    <Box />
+    <Box variants={myVars} initial="start" animate="end"/>
+   
   </Wrapper>
   );
 
