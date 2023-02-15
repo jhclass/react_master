0e1f78fdc5ca46d581c9bf04639d2a863c98f4e1
Jinhyoung Lee <jhcode0@naver.com>
2023-02-15 15:44:42 +0900

    슬라이더 만들어 보기

--- src/App.tsx
+++ src/App.tsx
@@ -10,36 +10,62 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
-
+  flex-direction: column;
   background-image:linear-gradient(135deg,#e09,#d0e);
 `;
 
 const Box = styled(motion.div)`
-   width: 400px;
+  width: 200px;
   height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
   position: absolute;
-  top: 100px;
+  top:100px;
+  display:flex;
+  align-items: center;
+  justify-content:center;
+  font-size:20px;
+  color:#000;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  initial:{opacity:0},
-  animate:{opacity:1,y:-10},
-  exit:{opacity:0,y:10}
+
+const box = {
+  invisible:{
+    x:500,
+    opacity:0,
+    scale:0,
+  },
+  visible:{
+    x:0,
+    opacity:1,
+    scale:1
+  },
+  exit:{
+    x:-500,
+    opacity:0,
+    scale:0
+  },
 }
 
 
 function App() {
-  const [showing,setShowing]= useState(false);
-  const toggleShowing = () => {
-    setShowing((prev)=> !prev);
-  }
+ const [visible,setVisible] = useState(1);
+ const next = () => setVisible((prev)=>(prev===9 ? 0:prev+1));
+
   return (
   <Wrapper>
-    <button onClick={toggleShowing}>Click</button>
-    <AnimatePresence>{showing?<Box variants={boxVariants} initial="initial" animate="animate" exit="exit"/>:null}</AnimatePresence>
+    <AnimatePresence>
+      {[0,1,2,3,4,5,6,7,8,9].map((t,i)=>
+        visible===i?<Box 
+        variants={box}
+        initial="invisible"
+        animate="visible"
+        exit="exit"
+        key={i}>{i}</Box>:null)
+      }
+    </AnimatePresence>
+    <button onClick={next}>next</button>
   </Wrapper>
   );
 
