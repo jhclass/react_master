df8c07ea7868bd0b60eb34f0045f952ebbee12c7
Jinhyoung Lee <jhcode0@naver.com>
2023-02-23 12:43:52 +0900

    motion whileHover inherit

--- src/Components/Boxes.tsx
+++ src/Components/Boxes.tsx
@@ -6,10 +6,11 @@ import { makeImagePath } from '../Utils';
 import { Tween } from 'jquery';
 
 const Box = styled(motion.div)<{bgImage:string}>`
+position:relative;
 background-image: url(${props=>props.bgImage});
 background-size:cover;
 height:250px;
-color:Red;
+color:black;
 font-size:64px;
 overflow:hidden;
 cursor: pointer;
@@ -17,9 +18,32 @@ cursor: pointer;
     transform-origin: center left;
 }
 &:last-child {
-    transform-origin:center right
+    transform-origin:center right;
 }
 `;
+
+const Info = styled(motion.div)`
+ padding:20px;
+ background-color:${props=>props.theme.black.lighter};
+ opacity: 0;
+ position:absolute;
+ width:100%;
+ bottom:0px;
+  h4 {text-align:center; color:#fff; font-size:18px;}
+`;
+
+const boxVariants = {
+    normal:{scale:1,y:0},
+    hover:{scale:1.3, y:-50,zIndex:999999,transition:{delay:0.3,type:Tween}},
+}
+
+const infoVariants = {
+   
+    hover:{
+        opacity:1
+    }
+}
+
 interface iMovieImages {
     id:number,
     title:string,
@@ -32,10 +56,16 @@ function Boxes ({id,title,description,index}:iMovieImages) {
     return (
         <Box
         bgImage={makeImagePath(data?.backdrops[index].file_path||"","w500")}
-        whileHover={{scale:1.3, y:-50,transition:{delay:0.3,type:Tween}}}
+        variants={boxVariants}
+        initial="normal"
+        whileHover="hover"
         transition={{type:"tween"}}
        
-        >{title}</Box>
+        >{/**이렇게 했더니 하위요소에 상속이 안되네.. */}
+            <Info variants={infoVariants}> 
+                <h4>{title}</h4>
+            </Info>
+        </Box>
     );
 }
 export default Boxes;
