df5f9e285c152927df5ed2e03b94a8d9ade86f8b
Jinhyoung Lee <jhcode0@naver.com>
2023-02-27 12:40:19 +0900

    URLSearchParams()

--- src/Routes/Search.tsx
+++ src/Routes/Search.tsx
@@ -1,4 +1,11 @@
+import { useLocation } from "react-router";
 function Search() {
-    return null
+    
+    const location = useLocation();
+    console.log(location);
+    const search = new URLSearchParams(location.search);
+    const keyword = search.get("keyword");
+    console.log('키워드만',keyword);
+    return null;
 }
-export default Search;
+export default Search; 
